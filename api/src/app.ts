9821a835253fb704d9dea2ffb48d684aaf044cea
Zaland Khan <zalandkhan1@gmail.com>
2022-04-28 13:45:25 -0700

    Move middleware back to app file

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,11 +1,21 @@
 import express from "express";
-import { initMiddleware } from "./middleware";
-import { initRoutes } from "./routes";
+import bodyParser from "body-parser";
+import PersonRouter from "./routes/Person.route";
 
 const app = express();
 
-// setup middlewares
-initMiddleware(app);
+// bodyparser
+app.use(bodyParser.json());
+
+// cors
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // setup endpoints
 initRoutes(app);
